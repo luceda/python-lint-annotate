0b002de93845c9354a624c3abde883124af5e56e
DennisVVV <dennis@lucedaphotonics.com>
2021-04-23 15:26:24 +0200

    only python3 files that are added in PR are checked

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@
 # ${16} - extra-vulture-options
 # ${17} - extra-pydocstyle-options
 
-echo python-root-list:          $1
+echo python-root-list:          $1  # useless now, replaced by $new_python_files_in_branch
 echo use-pylint:                $2
 echo use-pycodestyle:           $3
 echo use-flake8:                $4
@@ -47,11 +47,55 @@ done
 echo "TERM: changing from $TERM -> xterm"
 export TERM=xterm
 
+
+find_base_commit() {
+    BASE_COMMIT=$(
+        jq \
+            --raw-output \
+            .pull_request.base.sha \
+            "$GITHUB_EVENT_PATH"
+    )
+    # If this is not a pull request action it can be a check suite re-requested
+    if [ "$BASE_COMMIT" == null ]; then
+        BASE_COMMIT=$(
+            jq \
+                --raw-output \
+                .check_suite.pull_requests[0].base.sha \
+                "$GITHUB_EVENT_PATH"
+        )
+    fi
+}
+
+find_base_commit
+
+# Find adjusted files in PR:
+new_files_in_branch=$(
+    git diff \
+        --name-only \
+        --diff-filter=AM \
+        "$BASE_COMMIT"
+)
+new_files_in_branch1=$(echo $new_files_in_branch | tr '\n' ' ')
+
+echo "New files in branch: $new_files_in_branch1"
+# Feed to flake8 which will return the output in json format.
+# shellcheck disable=SC2086
+if [[ $new_files_in_branch =~ .*".py".* ]]; then
+    new_python_files_in_branch=$(
+        git diff \
+            --name-only \
+            --diff-filter=AM \
+            "$BASE_COMMIT" | grep '\.py$' | grep 'python3' | tr '\n' ' '
+    )
+    echo "New python3 files in branch: $new_python_files_in_branch"
+fi
+
+
 if [ "$2" = true ] ; then
 
-    echo Running: pylint ${10} $1
+    echo Running: pylint ${10} $new_python_files_in_branch
 
-    $CONDA/bin/pylint --output-format="colorized" ${10} $1
+    $CONDA/bin/pylint --output-format="colorized" ${10} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -64,9 +108,9 @@ fi
 
 if [ "$3" = true ] ; then
 
-    echo Running: pycodestyle ${11} $1
+    echo Running: pycodestyle ${11} $new_python_files_in_branch
 
-    $CONDA/bin/pycodestyle ${11} $1
+    $CONDA/bin/pycodestyle ${11} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -79,9 +123,9 @@ fi
 
 if [ "$4" = true ] ; then
 
-    echo Running: flake8 ${12} $1
+    echo Running: flake8 ${12} $new_python_files_in_branch
 
-    $CONDA/bin/flake8 ${12} $1
+    $CONDA/bin/flake8 ${12} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -94,9 +138,9 @@ fi
 
 if [ "$5" = true ] ; then
 
-    echo Running: black --check ${13} $1
+    echo Running: black --check ${13} $new_python_files_in_branch
 
-    $CONDA/bin/black --check ${13} $1
+    $CONDA/bin/black --check ${13} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -109,9 +153,9 @@ fi
 
 if [ "$6" = true ] ; then
 
-    echo Running: mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers ${14} $1
+    echo Running: mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers ${14} $new_python_files_in_branch
 
-    $CONDA/bin/mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers ${14} $1
+    $CONDA/bin/mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers ${14} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -124,9 +168,9 @@ fi
 
 if [ "$7" = true ] ; then
 
-    echo Running: isort ${15} $1 -c --diff
+    echo Running: isort ${15} $new_python_files_in_branch -c --diff
 
-    $CONDA/bin/isort ${15} $1 -c --diff
+    $CONDA/bin/isort ${15} $new_python_files_in_branch -c --diff
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -139,9 +183,9 @@ fi
 
 if [ "$8" = true ] ; then
 
-    echo Running: vulture ${16} $1
+    echo Running: vulture ${16} $new_python_files_in_branch
 
-    $CONDA/bin/vulture ${16} $1
+    $CONDA/bin/vulture ${16} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
@@ -154,9 +198,9 @@ fi
 
 if [ "$9" = true ] ; then
 
-    echo Running: pydocstyle ${17} $1
+    echo Running: pydocstyle ${17} $new_python_files_in_branch
 
-    $CONDA/bin/pydocstyle ${17} $1
+    $CONDA/bin/pydocstyle ${17} $new_python_files_in_branch
     exit_code=$?
 
     if [ "$exit_code" = "0" ]; then
